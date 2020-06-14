0af7d09d1e07f5985a2a41c4966552315911bcd0
ramakrishnamb <ramakrishnamb@gmail.com>
2020-06-14 23:20:44 +0530

    Add project files.

---
+++ DapperAdvanceDemoLibrary/Tools.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Text;
+
+namespace ModelsLibrary
+{
+    public static class Tools
+    {
+        public static string GetConnectionString(string name = "DapperDB")
+        {
+            //return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+
+            return "Data Source=DESKTOP-LBTN8H4\\SQLEXPRESS2019;Initial Catalog=DapperDBRKDemo;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        }
+    }
+}
